409507c052dc8d90642629fba5ff3de4e7e28ee3
Stephen Stewart <stephen.stewart@carisenda.com>
2017-03-31 21:49:55 +0100

    tidy up repostory reducers

---
+++ src/common/reducers/entities.js
@@ -0,0 +1,62 @@
+import merge from 'lodash/merge';
+
+import * as ActionTypes from '../actions/repositories';
+
+// export only for testing
+export function repository(state={
+  __isSelected: false,
+  __isFetching: false,
+  __error: null
+}, action) {
+
+  switch(action.type) {
+    case ActionTypes.REPOSITORY_TOGGLE_SELECT: {
+      const wasSelected = state.__isSelected;
+
+      return {
+        ...state,
+        __isSelected: !wasSelected
+      };
+    }
+    case ActionTypes.REPOSITORY_BUILD: {
+      return {
+        ...state,
+        __isFetching: true,
+        __isSelected: true
+      };
+    }
+    case ActionTypes.REPOSITORY_SUCCESS:
+    case ActionTypes.REPOSITORY_RESET: {
+      return {
+        ...state,
+        __isSelected: false,
+        __isFetching: false,
+        __error: null
+      };
+    }
+    case ActionTypes.REPOSITORY_FAILURE: {
+      return {
+        ...state,
+        __error: action.payload.error.json
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+// TODO snaps
+
+export function entities(state = { snaps: {}, repos: {} }, action) {
+  if (action.payload && action.payload.entities) {
+    return merge({}, state, action.payload.entities);
+  }
+
+  return {
+    ...state,
+    repos: {
+      ...state.repos,
+      [action.payload.id]: repository(state.repos[action.payload.id], action)
+    }
+  };
+}
--- src/common/reducers/repositories.js
+++ src/common/reducers/repositories.js
@@ -1,85 +1,6 @@
-import merge from 'lodash/merge';
 import union from 'lodash/union';
 
 import * as ActionTypes from '../actions/repositories';
-
-// XXX move to entities.js
-// XXX should we be handling snaps here too?
-// no, because it's not part of the normalized result from github
-// so this should really be called github_entities
-// no, decompose instead ...
-export function entities(state = { snaps: {}, repos: {} }, action) {
-  if (action.payload && action.payload.entities) {
-    return merge({}, state, action.payload.entities);
-  }
-
-
-  const reposInitialState = {
-    __isSelected: false,
-    __isFetching: false,
-    __error: null
-  };
-
-  switch(action.type) {
-    case ActionTypes.REPOSITORY_TOGGLE_SELECT: {
-      const wasSelected = state.repos[action.payload.id].__isSelected;
-
-      return {
-        ...state,
-        repos: {
-          ...state.repos,
-          [action.payload.id]: {
-            ...state.repos[action.payload.id] || {},
-            ...reposInitialState, // clear state from building by deselecting
-            __isSelected: !wasSelected
-          }
-        }
-      };
-    }
-    case ActionTypes.REPOSITORY_BUILD: {
-      return {
-        ...state,
-        repos: {
-          ...state.repos,
-          [action.payload.id]: {
-            ...state.repos[action.payload.id] || {},
-            ...reposInitialState,
-            __isFetching: true,
-            __isSelected: true
-          }
-        }
-      };
-    }
-    case ActionTypes.REPOSITORY_SUCCESS:
-    case ActionTypes.REPOSITORY_RESET: {
-      return {
-        ...state,
-        repos: {
-          ...state.repos,
-          [action.payload.id]: {
-            ...state.repos[action.payload.id] || {},
-            ...reposInitialState
-          }
-        }
-      };
-    }
-    case ActionTypes.REPOSITORY_FAILURE: {
-      return {
-        ...state,
-        repos: {
-          ...state.repos,
-          [action.payload.id]: {
-            ...state.repos[action.payload.id] || {},
-            ...reposInitialState,
-            __error: action.payload.error.json
-          }
-        }
-      };
-    }
-  }
-
-  return state;
-}
 
 export function repositories(state = {
   isFetching: false,
@@ -107,11 +28,6 @@ export function repositories(state = {
         ...state,
         isFetching: false,
         error: action.payload,
-      };
-    case ActionTypes.REPOSITORIES_SELECT_CLEAR:
-      return {
-        ...state,
-        selected: []
       };
     default:
       return state;
