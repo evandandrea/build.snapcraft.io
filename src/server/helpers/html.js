df758efeeda656b6286c1da53a9aa04022bb8227
Evan <evandandrea@users.noreply.github.com>
2017-10-06 10:30:40 -0400

    Add missing auto to GA setup

--- src/server/helpers/html.js
+++ src/server/helpers/html.js
@@ -33,7 +33,7 @@ const googleAnalytics = GAID && OPTID ?
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', '${GAID}', {'allowLinker': true});
+    ga('create', '${GAID}', 'auto', {'allowLinker': true});
     ga('require', 'linker');
     ga('linker:autoLink', ['snapcraft.io', 'build.snapcraft.io', 'dashboard.snapcraft.io',
     'conjure-up.io', 'login.ubuntu.com', 'www.ubuntu.com', 'ubuntu.com', 'insights.ubuntu.com',
